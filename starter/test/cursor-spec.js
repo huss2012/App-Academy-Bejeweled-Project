108e5b766c984a986da062e1015e543edefad61f
huss2012 <huss24940@gmail.com>
2024-05-06 09:33:18 +0300

    Curser test specs are updated.

--- starter/test/cursor-spec.js
+++ starter/test/cursor-spec.js
@@ -1,13 +1,14 @@
 const { expect } = require('chai');
+const chai = require('chai');
 
 const Cursor = require("../class/cursor.js");
 const Screen = require("../class/screen.js");
 
-describe ('Cursor', function () {
+describe('Cursor', function () {
 
   let cursor;
 
-  beforeEach(function() {
+  beforeEach(function () {
     cursor = new Cursor(3, 3);
   });
 
@@ -65,7 +66,106 @@ describe ('Cursor', function () {
     expect([cursor.row, cursor.col]).to.deep.equal([0, 0]);
   });
 
+  describe('swapFruits(grid, fruit1, fruit2)', () => {
+    context('if the two fruits are adjacents', () => {
+      it('should swap fruits positions', () => {
+        grid =
+          [[" ", " ", " ", " ", " ", " ", " ", " "],
+          [" ", " ", " ", " ", " ", " ", " ", " "],
+          [" ", " ", " ", " ", " ", " ", " ", " "],
+          [" ", " ", " ", " ", " ", " ", " ", " "],
+          [" ", " ", " ", " ", " ", " ", " ", " "],
+          [" ", " ", " ", " ", " ", " ", " ", " "],
+          [" ", " ", " ", " ", " ", " ", " ", " "],
+          [" ", " ", "🥭", "🥭", "🥥", "🥭", " ", " "]];
 
+        let fruit1 = { row: 7, col: 5 };
+        let fruit2 = { row: 7, col: 4 };
 
+        Cursor.swapFruits(grid, fruit1, fruit2);
+
+        expect(grid[7][5]).to.equal("🥥");
+        expect(grid[7][4]).to.equal("🥭");
+
+        grid =
+          [[" ", " ", " ", " ", " ", " ", " ", " "],
+          [" ", " ", " ", " ", " ", " ", " ", " "],
+          [" ", " ", " ", " ", " ", " ", " ", " "],
+          [" ", " ", " ", " ", " ", " ", " ", " "],
+          [" ", "🍓", " ", " ", " ", " ", " ", " "],
+          [" ", "🫐", " ", " ", " ", " ", " ", " "],
+          [" ", "🍓", " ", " ", " ", " ", " ", " "],
+          [" ", "🍓", " ", " ", " ", " ", " ", " "]];
+
+        fruit1 = { row: 5, col: 1 };
+        fruit2 = { row: 4, col: 1 };
+
+        Cursor.swapFruits(grid, fruit1, fruit2);
+
+        expect(grid[5][1]).to.equal("🍓");
+        expect(grid[4][1]).to.equal("🫐");
+      });
+    });
+
+    context('if the two fruits are not adjacent', () => {
+      it('should does not swap the fruits', () => {
+        grid =
+          [[" ", " ", " ", " ", " ", " ", " ", " "],
+          [" ", "🫐", " ", " ", " ", " ", " ", " "],
+          [" ", " ", " ", " ", "🍓", " ", " ", " "],
+          [" ", " ", " ", " ", " ", " ", " ", " "],
+          [" ", " ", " ", " ", " ", " ", " ", " "],
+          [" ", " ", " ", " ", " ", " ", " ", " "],
+          [" ", " ", " ", " ", " ", " ", " ", " "],
+          [" ", " ", " ", " ", " ", " ", " ", " "]];
+
+        let fruit1 = { row: 1, col: 1 };
+        let fruit2 = { row: 2, col: 4 };
+
+        Cursor.swapFruits(grid, fruit1, fruit2);
+
+        expect(grid[fruit1.row][fruit1.col]).to.equal("🫐");
+        expect(grid[fruit2.row][fruit2.col]).to.equal("🍓");
+      });
+    });
+  })
+
+  describe('selectFruit(grid, selection, fruit)', () => {
+    it('should select a fruit from the grid, pushing it to a selection array', () => {
+      grid = [[" ", " ", " ", " ", " ", " ", "🥝", " "],
+      [" ", " ", " ", " ", " ", " ", " ", " "],
+      [" ", " ", " ", " ", " ", " ", " ", " "],
+      [" ", " ", " ", " ", " ", " ", " ", " "],
+      [" ", " ", " ", " ", " ", " ", " ", " "],
+      [" ", " ", " ", " ", " ", " ", " ", " "],
+      [" ", " ", " ", " ", " ", " ", " ", " "],
+      [" ", " ", " ", " ", " ", " ", " ", " "]];
+
+      cursor.selection = [];
+      cursor.selectFruit(grid, { row: 0, col: 6 });
+      expect(cursor.selection).to.deep.equal([{ row: 0, col: 6 }]);
+    });
+
+    // context('if two fruits are selected', () => {
+    //   it('should call swapFruits method', () => {
+    //     grid = [[" ", " ", " ", " ", " ", " ", " ", " "],
+    //     [" ", " ", " ", " ", " ", " ", " ", " "],
+    //     [" ", " ", " ", " ", " ", " ", " ", " "],
+    //     [" ", " ", " ", " ", " ", " ", " ", " "],
+    //     [" ", " ", " ", " ", " ", " ", " ", " "],
+    //     [" ", " ", " ", " ", " ", " ", " ", " "],
+    //     [" ", " ", " ", " ", " ", " ", " ", " "],
+    //     [" ", " ", " ", "🍊", "🍊", "🥝", "🍊", " "]];
+
+    //     let fruit1 = {row: 7, col: 6};
+    //     let fruit2 = {row: 7, col: 5};
+
+    //     cursor.selection = [fruit1];
+    //     let spySwap = chai.spy.on(Cursor, 'swapFruits');
+    //     cursor.selectFruit(grid, fruit2);
+    //     expect(spySwap).to.have.been.called;
+    //     expect(grid[fruit1.row][fruit1.col]).to.equal("🥝");
+    //   });
+    // });
+  });
 });
-
